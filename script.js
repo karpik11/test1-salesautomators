8448092d890d4ca0e272c28c5aaa5ca0fa6adeb8
karpik11 <163449991+karpik11@users.noreply.github.com>
2024-11-26 00:45:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,59 @@
-'use strict';
+'use strict'
 
-async function submitForm() {
-  const data = {
-    first_name: document.getElementById('firstName').value,
-    last_name: document.getElementById('lastName').value,
-    phone: document.getElementById('phone').value,
-    email: document.getElementById('email').value,
-    job_type: document.getElementById('jobType').value,
-    job_source: document.getElementById('jobSource').value,
-    job_description: document.getElementById('jobDescription').value,
-    address: document.getElementById('address').value,
-    city: document.getElementById('city').value,
-    state: document.getElementById('state').value,
-    zip_code: document.getElementById('zipCode').value,
-    area: document.getElementById('area').value,
-    start_date: document.getElementById('startDate').value,
-    start_time: document.getElementById('startTime').value,
-    end_time: document.getElementById('endTime').value,
-    test_select: document.getElementById('testSelect').value
-  };
+$(document).ready(function () {
+  $('#dealForm').submit(function (event) {
+    event.preventDefault();
 
-  const response = await fetch('https://api.pipedrive.com/v1/deals?api_token=39f7d61ddc010077e3adc1ec0111939a64e08192', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data)
+    const firstName = $('#firstName').val();
+    const lastName = $('#lastName').val();
+    const phone = $('#phone').val();
+    const email = $('#email').val();
+    const jobType = $('#jobType').val();
+    const jobSource = $('#jobSource').val();
+    const jobDescription = $('#jobDescription').val();
+    const address = $('#address').val();
+    const city = $('#city').val();
+    const state = $('#state').val();
+    const zipCode = $('#zipCode').val();
+    const area = $('#area').val();
+    const startDate = $('#startDate').val();
+    const startTime = $('#startTime').val();
+    const endTime = $('#endTime').val();
+    const testSelect = $('#testSelect').val();
+    const dealId = 3;
+
+    $.ajax({
+      url: `https://api.pipedrive.com/v1/deals/${dealId}?api_token=bc761f15df4310d357ab3e90ecff397b7b20021f`,
+      type: 'PUT',
+      contentType: 'application/json',
+      data: JSON.stringify({
+
+        "6ede13b871449bff49086658859194ee61090ddb": firstName,
+        "f703b5a9f5c7df91d9069af489b45723cfd3e009": lastName,
+        "7c30d180c6383f4aa851ec700c64e3318d87f7e2": phone,
+        "97c331443e88bfa7211c8e83e20b6abdf71d2b57": email,
+        "7b9e6516fa2ea636e52da831c0c7a2899852f9aa": jobType,
+        "81e2138a65fc90393917c06a2dbd2da02d3aabd8": jobSource,
+        "bc0eadfdef1d74db0e863a91896642d98c605a36": jobDescription,
+        "a12fe72fbe5a6d0cac017e1be3436d6cdf72a072": address,
+        "3f32b605b09f97d99c00adef5e2d5950d654144b": city,
+        "1f0393b84ec021b3c49c0ed438664104f7ea8e7b": state,
+        "47af53c284ae27764f6882e73a0e8e545ca30407": zipCode,
+        "b91b347aa2ed4fa4cb7f6a4f14db3a15214148b6": area,
+        "e061e6a0eebbe8887c8588731ae37d079aa347f0": startDate,
+        "70ff2ef43fc5b804259eb935f443979e371d21c9": startTime,
+        "0819168a3f666676e50f10f1e43264f5d74a0e28": endTime,
+        "42166328a7392a8c6267594fd084690d2833e46b": testSelect,
+
+      }),
+      success: function (response) {
+        alert('Job is created');
+
+        $('#dealForm')[0].reset();
+      },
+      error: function (xhr) {
+        alert('Error: ' + xhr.responseText);
+      }
+    });
   });
-
-  // if (response.ok) {
-  //   alert('Deal created successfully!');
-  // } else {
-  //   alert('Error creating deal');
-  // }
-}
+});
